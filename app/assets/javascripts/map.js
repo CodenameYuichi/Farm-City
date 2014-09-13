9a32edabc6a8f2f8088f03d19347dafb77570535
Grzegorz Lisowski <glisowski@fractalsoft.org>
2014-09-13 04:59:00 +0200

    added Google map

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,17 +1,11 @@
-handler = Gmaps.build('Google');
-handler.buildMap({ provider: {}, internal: {id: 'map'}}, function(){
-  markers = handler.addMarkers([
-    {
-      "lat": 0,
-      "lng": 0,
-      "picture": {
-        "url": "https://addons.cdn.mozilla.net/img/uploads/addon_icons/13/13028-64.png",
-        "width":  36,
-        "height": 36
-      },
-      "infowindow": "hello!"
-    }
-  ]);
-  handler.bounds.extendWith(markers);
-  handler.fitMapToBounds();
+$(function() {
+  if($('#map').length != 0) {
+    handler = Gmaps.build('Google');
+    handler.buildMap({ internal: {id: 'map'}}, function(){
+      var markers = handler.addMarkers(nodes);
+      handler.bounds.extendWith(markers);
+      handler.fitMapToBounds();
+      }
+    );
+  }
 });
