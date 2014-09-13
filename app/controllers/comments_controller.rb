3bd4aae66fc2a9a4d12ea6a5d1f6ccbcc3f1302a
Grzegorz Lisowski <glisowski@fractalsoft.org>
2014-09-13 22:57:01 +0200

    site routing

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -47,10 +47,7 @@ class CommentsController < ApplicationController
   # DELETE /comments/1.json
   def destroy
     @comment.destroy
-    respond_to do |format|
-      format.html { redirect_to comments_url, notice: 'Comment was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to place_path(params[:place_id])
   end
 
   private
