20a279b606ecfc69fbc91e6c64c0f247bbb03f1b
YuichiTK <pfitzner@fractalsoft.org>
2014-09-13 05:26:59 +0200

    link to

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,13 @@ Bundler.require(*Rails.groups)
 
 module Farmcity
   class Application < Rails::Application
+    config.to_prepare do
+        Devise::SessionsController.layout "home"
+        Devise::RegistrationsController.layout proc{ |controller| user_signed_in? ? "application" : "home" }
+        Devise::ConfirmationsController.layout "home"
+        Devise::UnlocksController.layout "home"
+        Devise::PasswordsController.layout "home"
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
