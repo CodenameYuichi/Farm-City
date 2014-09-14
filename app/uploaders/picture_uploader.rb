557e7f4be882e8c76feb31ed4f38ca85c310d1df
Aleksander Malaszkiewicz <alek@fractalsoft.org>
2014-09-14 10:07:34 +0200

    Use bootstrap thumbnails

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -1,17 +1,8 @@
-# encoding: utf-8
-
 class PictureUploader < CarrierWave::Uploader::Base
-
-  # Include RMagick or MiniMagick support:
   include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
 
-  # Choose what kind of storage to use for this uploader:
   storage :file
-  # storage :fog
 
-  # Override the directory where uploaded files will be stored.
-  # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
@@ -24,36 +15,19 @@ class PictureUploader < CarrierWave::Uploader::Base
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
   # end
 
-  # Process files as they are uploaded:
-  # process :scale => [200, 300]
-  #
-  # def scale(width, height)
-  #   # do something
-  # end
-
-  # Create different versions of your uploaded files:
   version :thumb do
-    process :resize_to_limit => [180, 180]
+    process resize_to_fill: [180, 180]
   end
 
   version :medium do
-    process :resize_to_limit => [280, 280]
+    process resize_to_fill: [280, 280]
   end
 
   version :large do
-    process :resize_to_limit => [320, 320]
+    process resize_to_fill: [320, 320]
   end
 
-  # Add a white list of extensions which are allowed to be uploaded.
-  # For images you might use something like this:
-  # def extension_white_list
-  #   %w(jpg jpeg gif png)
-  # end
-
-  # Override the filename of the uploaded files:
-  # Avoid using model.id or version_name here, see uploader/store.rb for details.
-  # def filename
-  #   "something.jpg" if original_filename
-  # end
-
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
 end
